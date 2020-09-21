97e949440386e2f0fa4a659a5840e7f1d7f96e9d
crrmacarse <crrmacarse@gmail.com>
2020-09-21 12:26:56 +0800

    feat: finalized ungeneric views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
 
 from .models import Question
 
@@ -17,7 +19,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': questiopn,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # purpose of F https://docs.djangoproject.com/en/3.1/ref/models/expressions/#avoiding-race-conditions-using-f
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after succesfully dealing with POST
+        # data. This prevents data from being posted twice if a user hits the
+        # Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
